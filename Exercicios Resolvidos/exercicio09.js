6caefb1f8b973e51e989f2bc552387521632f588
Matheus Lima <matheuslimacarneirors@gmail.com>
2024-08-15 14:37:34 -0300

    exercicio 9 concluido

---
+++ Exercicios Resolvidos/exercicio09.js
@@ -0,0 +1,11 @@
+/*
+Implemente um programa que exibe uma contagem regressiva de 10 até 1 no console
+utilizando um loop for.
+*/
+
+let contagem = 0
+
+for(let i =10; i >= 1; i-- ){
+    contagem = i
+    console.log(contagem)
+}
