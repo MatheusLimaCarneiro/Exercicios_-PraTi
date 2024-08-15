ccd7198853a4656bd5a936ae3b7f3b3937ca082d
Matheus Lima <matheuslimacarneirors@gmail.com>
2024-08-15 14:47:19 -0300

    exercicio 10 concluido

---
+++ Exercicios Resolvidos/exercicio10.js
@@ -0,0 +1,11 @@
+/*
+Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
+*/
+
+const prompt = require('prompt-sync')()
+
+let numero = Number(prompt('Esqreva um numero: '))
+
+for(let i = 0; let <= 10; i++){
+    console.log(numero)
+}
